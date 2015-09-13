023ec6b4d3d1a65730535990628e0027df07ba8a
Eric Dennison <ericd@netdenizen.com>
2015-09-12 20:54:31 -0400

    Updated from Brython Server: 9/12/2015 8:54:28 PM

--- tutorial2.py
+++ tutorial2.py
@@ -1,23 +1,18 @@
 from ggame import App, Color, LineStyle, Sprite
 from ggame import CircleAsset
-from math import sin, cos, radians, degrees
 
-
-# Three primary colors with no transparency (alpha = 1.0)
 red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 
-# Define a line style that is a thin (1 pixel) wide black line
 thinline = LineStyle(1, black)
-bluecircle = CircleAsset(5, thinline, blue)
-redcircle = CircleAsset(5, thinline, red)
-[Sprite(bluecircle, (a, sin(radians(a))*100+100)) for a in range(0,360,5)]
-[Sprite(redcircle, (a, cos(radians(a))*100+100)) for a in range(0,360,5)]
+mycircle = CircleAsset(5, thinline, blue)
+xcoordinates = range(0, 360, 10)
 
-def step():
-    pass
+# Generate a list of sprites that form a line!
+sprites = [Sprite(mycircle, (x, x*2.5 + 40)) for x in xcoordinates]
+
 
 myapp = App()
-myapp.run(step)
+myapp.run()
