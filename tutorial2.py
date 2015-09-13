f82c5920e49fcee271b385da61d20d331df0cfc3
Eric Dennison <ericd@netdenizen.com>
2015-09-12 22:11:59 -0400

    Updated from Brython Server: 9/12/2015 10:11:57 PM

--- tutorial2.py
+++ tutorial2.py
@@ -6,15 +6,18 @@ red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
+purple = Color(0x00ffff, 1.0)
 
 thinline = LineStyle(1, black)
 mycircle = CircleAsset(5, thinline, blue)
 xcoordinates = range(0, 360, 10)
 redcircle = CircleAsset(5, thinline, red)
+purcircle = CircleAsset(5, thinline, purple)
 
 # Generate a list of sprites that form a line!
 blsprites = [Sprite(mycircle, (x, 100+100*math.sin(math.radians(x)))) for x in xcoordinates]
 resprites = [Sprite(redcircle, (x, 100+100*math.cos(math.radians(x)))) for x in xcoordinates]
+pursprites = [Sprite(pucircle, (100+100*math.cos(math.radians(x))), 100+100*math.cos(math.radians(x)))) for x in xcoordinates]
 
 myapp = App()
 myapp.run()
