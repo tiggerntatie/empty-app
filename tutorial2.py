2b7c692edf46eca5d43acdba8df70191a59a09c1
Eric Dennison <ericd@netdenizen.com>
2015-09-12 20:22:11 -0400

    Updated from Brython Server: 9/12/2015 8:22:08 PM

--- tutorial2.py
+++ tutorial2.py
@@ -1,8 +1,7 @@
 from ggame import App, Color, LineStyle, Sprite
-from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
-from random import randint, seed
+from ggame import CircleAsset
+from math import sin, cos, radians, degrees
 
-seed()
 
 # Three primary colors with no transparency (alpha = 1.0)
 red = Color(0xff0000, 1.0)
@@ -12,15 +11,13 @@ black = Color(0x000000, 1.0)
 
 # Define a line style that is a thin (1 pixel) wide black line
 thinline = LineStyle(1, black)
-myrectangle = RectangleAsset(50, 50, thinline, green)
-mysprites = [Sprite(myrectangle, (x*randint(2,10), x*randint(2,10))) for x in range(100)]
+mycircle = CircleAsset(5, thinline, blue)
+[Sprite(mycircle, (a, sin(radians(a))*100+50)) for a in range(0,360,5)]
+#mysprites = [Sprite(mycircle, (x*randint(2,10), x*randint(2,10))) for x in range(100)]
 #mysprite = Sprite(myrectangle, (300, 300))
 
 def step():
-    dx = randint(-5,5)
-    dy = randint(-5,5)
-    mysprites[50].x += dx
-    mysprites[50].y += dy
+    pass
 
 myapp = App()
 myapp.run(step)
