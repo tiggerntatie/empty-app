4540cb83c2ff9e0b4e3bf5b7b7282c99b624279e
Eric Dennison <ericd@netdenizen.com>
2015-09-12 22:16:06 -0400

    Updated from Brython Server: 9/12/2015 10:16:04 PM

--- tutorial2.py
+++ tutorial2.py
@@ -6,7 +6,7 @@ red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
-purple = Color(0x00ffff, 1.0)
+purple = Color(0x551a8b, 1.0)
 
 thinline = LineStyle(1, black)
 mycircle = CircleAsset(5, thinline, blue)
